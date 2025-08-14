2cbb2409100d52b20b0341a16550f1e39e99e8ec
Matej2 <11059438+matej2@users.noreply.github.com>
2025-08-14 09:33:43 +0200

    Motorcycle class

--- Java-complete/src/main/java/org/example/vehicle/Motorcycle.java
+++ Java-complete/src/main/java/org/example/vehicle/Motorcycle.java
@@ -1,4 +1,20 @@
 package org.example.vehicle;
 
-public class Motorcycle {
+public class Motorcycle extends Vehicle implements Driveable{
+    short weightKg;
+
+    public Motorcycle(String make, String model, short year, short weightKg) {
+        super(make, model, year);
+        this.weightKg = weightKg;
+    }
+
+    @Override
+    public void start() {
+        System.out.println("Motorcycle started");
+    }
+
+    @Override
+    public void stop() {
+        System.out.println("Motorcycle stopped");
+    }
 }
