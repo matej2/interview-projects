6ea4eb81af5bcbc925ef4db32483a178524bd2a4
matej2 <11059438+matej2@users.noreply.github.com>
2025-09-22 15:47:08 +0200

    Refactored API to use service

--- postgredatabase/src/main/java/com/example/postgredatabase/controller/AuthorController.java
+++ postgredatabase/src/main/java/com/example/postgredatabase/controller/AuthorController.java
@@ -2,26 +2,29 @@ package com.example.postgredatabase.controller;
 
 import com.example.postgredatabase.domain.Author;
 import com.example.postgredatabase.repositories.AuthorRepository;
+import com.example.postgredatabase.service.AuthorService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
-@RequestMapping("api/v1/author")
+@RequestMapping("api/v1/authors")
 public class AuthorController {
     private final AuthorRepository authorRepository;
+    private final AuthorService authorService;
 
     @Autowired
-    public AuthorController(AuthorRepository authorRepository) {
+    public AuthorController(AuthorRepository authorRepository, AuthorService authorService) {
         this.authorRepository = authorRepository;
+        this.authorService = authorService;
     }
 
     @GetMapping
     public Iterable<Author> getAuthor() {
-        return authorRepository.findAll();
+        return authorService.getAllAuthors();
     }
 
     @PostMapping
     public void createAuthor(@RequestBody Author author) {
-        authorRepository.save(author);
+        authorService.createAuthor(author);
     }
 }
--- postgredatabase/src/main/java/com/example/postgredatabase/service/AuthorService.java
+++ postgredatabase/src/main/java/com/example/postgredatabase/service/AuthorService.java
@@ -1,4 +1,24 @@
 package com.example.postgredatabase.service;
 
+import com.example.postgredatabase.domain.Author;
+import com.example.postgredatabase.repositories.AuthorRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
 public class AuthorService {
+    private AuthorRepository authorRepository;
+
+    @Autowired
+    public AuthorService(AuthorRepository authorRepository) {
+        this.authorRepository = authorRepository;
+    }
+
+    public Iterable<Author> getAllAuthors() {
+        return authorRepository.findAll();
+    }
+
+    public void createAuthor(Author author) {
+        authorRepository.save(author);
+    }
 }
