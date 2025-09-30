75f35dbab4713483b90f3ac20e06f6a1aafb52ef
matej2 <11059438+matej2@users.noreply.github.com>
2025-09-30 12:46:17 +0200

    Removed unused code

--- doctor-file-processor/src/test/java/com/doctor/file_processor/TestFileProcessorApplication.java
+++ doctor-file-processor/src/test/java/com/doctor/file_processor/TestFileProcessorApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.SpringApplication;
 public class TestFileProcessorApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.from(FileProcessorApplication::main).with(TestcontainersConfiguration.class).run(args);
+		SpringApplication.from(FileProcessorApplication::main).run(args);
 	}
 
 }
